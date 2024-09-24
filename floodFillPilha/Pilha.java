127047d7d370354c1389d2ebc883f9faef549835
kauan <kauanpardini@gmail.com>
2024-09-24 05:01:44 -0300

    Feito emm Pilha

--- floodFillPilha/Pilha.java
+++ floodFillPilha/Pilha.java
@@ -1,5 +1,39 @@
 package floodFillPilha;
 
-public class Pilha {
-    
+public class Pilha<T> {
+    private int top = -1;
+    private T[] data;
+
+    @SuppressWarnings("unchecked")
+    public Pilha (int size) {
+        this.data = (T[]) new Object[size];
+    }
+
+    public void push(T data) {
+        this.data[top++] = data;
+    }
+
+    public T pop() {
+        if (isEmpty())
+            return null;
+        T v = data[top];
+        data[top--] = null;
+        return v;
+    }
+
+    public void clear() {
+        if (isEmpty())
+            throw new IllegalArgumentException("Pilha está vazia!");
+        for(T data: this.data)
+            this.data[top--] = null;
+        top = -1;
+    }
+
+    public boolean isFull() {
+        return top + 1 == data.length;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
 }
